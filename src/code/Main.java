8ca7dde2fffd84449df847ae7bf9adad48578eab
sevcik3 <sevcik3@PC547.SPSEJECNANET.CZ>
2018-03-20 10:43:22 +0100

    Windows into gui package init code package

--- src/code/Main.java
+++ src/code/Main.java
@@ -1,43 +1,14 @@
 package code;
 
-import java.awt.EventQueue;
-
-import javax.swing.JFrame;
-
 public class Main {
 
-	private JFrame frame;
-
-	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					Main window = new Main();
-					window.frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
-
-	/**
-	 * Create the application.
-	 */
 	public Main() {
-		initialize();
+		// TODO Auto-generated constructor stub
 	}
 
-	/**
-	 * Initialize the contents of the frame.
-	 */
-	private void initialize() {
-		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Init code");
 	}
 
 }
